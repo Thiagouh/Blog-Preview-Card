85bba06aeb821d373388a682a35675c945303200
Thiagouh <139012325+Thiagouh@users.noreply.github.com>
2024-06-16 19:50:08 -0300

    Move index.html to root and adjust Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,css}", "./*.{html,js,css}"],
   theme: {
     screens: {
       'sm': '440px',
